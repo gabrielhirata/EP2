3ebc0d502092b57426c2d88fc8091d57a02fc222
Gabriel Hirata <gabrielhirata10@gmail.com>
2019-04-23 22:59:46 -0300

    começo da desgraça

---
+++ EP2.py
@@ -0,0 +1,17 @@
+# EP2  Gabriel Yudi Hirata  9349666
+
+class Grafo:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def AcrescentaVertice(v):
+        
+    # def RemoveVertice(v):
+    #     del v
+
+    # def AcrescentaArco(v1,v2,w):        
+    
+    # def RemoveArco(v1,v2):
+        
+    # def ImprimeGrafo():    
+
