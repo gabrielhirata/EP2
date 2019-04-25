030c19420dd2671a8a074926ce5d0408545d69c6
Gabriel Hirata <gabrielhirata10@gmail.com>
2019-04-25 17:37:41 -0300

    remove e adiciona vértices

--- EP2.py
+++ EP2.py
@@ -1,14 +1,24 @@
 # EP2  Gabriel Yudi Hirata  9349666
-
 class Grafo:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def AcrescentaVertice(v):
+    def __init__(self):
+        self.grafo = {}
+        self.vertice = {}
+        self.arco = []
         
-    # def RemoveVertice(v):
-    #     del v
+    def AcrescentaVertice(self,v):
+        if v in self.grafo:
+            print("%s já existe." %v)
+        else:
+            self.grafo[v] = {}
 
+    def RemoveVertice(self,v):
+        if v not in self.grafo:
+            print("%s não é um vértice existente." %v)
+        elif self.arco[v] == []:
+            self.vertice.pop(v)
+        else:
+            print("O vértice %s contém arco(s) e não pode ser removido." %v)
+            
     # def AcrescentaArco(v1,v2,w):        
     
     # def RemoveArco(v1,v2):
