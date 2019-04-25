437aae13c04d5b273a378468f1e10176f9a6e89f
Gabriel Hirata <gabrielhirata10@gmail.com>
2019-04-25 18:06:27 -0300

    remove e adiciona vertice e arco feitos

--- EP2.py
+++ EP2.py
@@ -1,27 +1,35 @@
 # EP2  Gabriel Yudi Hirata  9349666
 class Grafo:
     def __init__(self):
-        self.grafo = {}
         self.vertice = {}
-        self.arco = []
+        self.arco = {}
+        self.custo = []
         
     def AcrescentaVertice(self,v):
-        if v in self.grafo:
+        if v in self.vertice:
             print("%s já existe." %v)
         else:
-            self.grafo[v] = {}
+            self.vertice[v] = {}
 
     def RemoveVertice(self,v):
-        if v not in self.grafo:
+        if v not in self.vertice:
             print("%s não é um vértice existente." %v)
-        elif self.arco[v] == []:
+        elif self.arco[v] == {}:
             self.vertice.pop(v)
         else:
             print("O vértice %s contém arco(s) e não pode ser removido." %v)
             
-    # def AcrescentaArco(v1,v2,w):        
-    
-    # def RemoveArco(v1,v2):
+    def AcrescentaArco(self,v1,v2,w):        
+        if v1 or v2 not in self.vertice:
+            print("%s ou %s não é vértice." %(v1,v2))
+        else:
+            self.vertice[v1].update({v2:w})
+
+    def RemoveArco(self,v1,v2):
+        if v2 not in self.vertice[v1]:
+            print("Arco entre %s e %s não existe." %(v1,v2))
+        else:
+            self.vertice[v1].pop(v2)
         
     # def ImprimeGrafo():    
 
